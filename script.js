2bc19f8eaf0a9c005a152ae53e43b3fb12a26163
Mauricio Rodrigues bispo <125503314+mauriciorodrigues123@users.noreply.github.com>
2024-12-13 19:56:04 -0300

    001

---
+++ script.js
@@ -0,0 +1,36 @@
+window.revelar = ScrollReveal({ reset: true })
+
+revelar.reveal('#card-1',
+    {
+        duration: 500,
+        delay: 500
+    })
+
+
+revelar.reveal('#card-2',
+    {
+        duration: 500,
+        delay: 1000
+    })
+
+
+revelar.reveal('#card-3',
+    {
+        duration: 500,
+        delay: 1100
+    })
+
+revelar.reveal('#card-4',
+    {
+        duration: 500,
+        delay: 1300
+    })
+
+revelar.reveal('#card-5',
+    {
+        duration: 500,
+        delay: 1300
+    })
+
+
+
